024b398ebd52742a07350167e05789155d540ddd
Toshihiro.Sekiguchi <s45c.ss400@gmail.com>
2018-07-18 10:23:18 +0900

    Page 145 1st

--- src/xunit.py
+++ src/xunit.py
@@ -1,7 +1,10 @@
+class WasRun:
+    def __init__(self, name):
+        self.wasRun = None
+    def testMethod(self):
+        pass
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
 print(test.wasRun)
-
-class WasRun:
-    pass
